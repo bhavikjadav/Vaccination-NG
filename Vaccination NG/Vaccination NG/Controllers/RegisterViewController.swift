dd6ca443ad7c6dae8e725b0d43dba0ca7b4943b3
Bhavik Jadav <bhavik.jadav89@gmail.com>
2023-07-31 15:47:45 +0530

    Working on Code Refector. All Done Ready to move on Link or OTP verification.

--- Vaccination NG/Vaccination NG/Controllers/RegisterViewController.swift
+++ Vaccination NG/Vaccination NG/Controllers/RegisterViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 import Planet
 import FirebaseAuth
 import FirebaseFirestore
-import ProgressHUD
 
 class RegisterViewController: UIViewController {
     
@@ -21,17 +20,22 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var reEnterPasswordField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
     
-    let emailIcon = UIImageView()
-    let userIcon = UIImageView()
-    let lockIcon = UIImageView()
-    let lockIcon2 = UIImageView()
-    
     let db = Firestore.firestore()
+    let loader = ProgressLoading()
+    
+    // Constants for alert titles and messages
+    enum AlertConstants {
+        static let alertTitle = "Alert"
+        static let invalidEmailMessage = "Email Address not found!"
+        static let invalidMobileNoMessage = "Enter valid Contact Number!"
+        static let chooseAStrongPasswordMessage = "Choose a Strong Password"
+        static let passwordDoesntMatchMessage = "Password doesn't match!"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ProgressHUD.dismiss()
+        loader.dismissLoading()
         
         emailTextField.delegate = self
         phoneNumberField.delegate = self
@@ -41,75 +45,92 @@ class RegisterViewController: UIViewController {
         countryCodeTextField.addTarget(self, action:#selector(textDidBeginEditing), for: UIControl.Event.editingDidBegin)
         
         // Loading Basic Designs
-        loadBasicDesigns()
+        let customDesignVC = CustomRegisterView()
+        customDesignVC.loadBasicDesigns(emailTextField, userNameTextField, passwordField, reEnterPasswordField, countryCodeTextField, phoneNumberField)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
         
         if let username = userNameTextField.text, let email = emailTextField.text, let countryCode = countryCodeTextField.text, let phoneNumber = phoneNumberField.text, let password = passwordField.text, let reEnteredPassword = reEnterPasswordField.text {
             
-            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-                if let e = error {
-                    
-                    if !email.validateEmailId() {
+            if reEnteredPassword != password {
+                
+                self.showAlert(title: AlertConstants.alertTitle, message: AlertConstants.passwordDoesntMatchMessage)
+                self.reEnterPasswordField.layer.cornerRadius = 08
+                self.reEnterPasswordField.layer.borderWidth = 3
+                self.reEnterPasswordField.layer.borderColor = K.colors.dangerColor
+                self.reEnterPasswordField.text = ""
+                self.reEnterPasswordField.placeholder = AlertConstants.passwordDoesntMatchMessage
+                
+            } else {
+                
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                    if let e = error {
                         
-                        self.openAlert(title: "Alert", message: "Invalid Email-Id", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                            print("Okay Clicked.")
-                        }])
-                        
-                        self.emailTextField.layer.cornerRadius = 08
-                        self.emailTextField.layer.borderWidth = 3
-                        self.emailTextField.layer.borderColor = K.colors.dangerColor
-                        self.emailTextField.text = ""
-                        self.emailTextField.placeholder = "Kindly Provide valid email"
-                        
-                    } else if phoneNumber.count < 9 {
-                        
-                        self.openAlert(title: "Alert", message: "Invalid Phone Number", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                            print("Okay Clicked.")
-                        }])
-                        
-                        self.phoneNumberField.layer.cornerRadius = 08
-                        self.phoneNumberField.layer.borderWidth = 3
-                        self.phoneNumberField.layer.borderColor = K.colors.dangerColor
-                        self.phoneNumberField.text = ""
-                        self.phoneNumberField.placeholder = "Kindly Provide valid phone number"
-                        
-                    } else if !password.validatePassword() {
-                        
-                        self.openAlert(title: "Alert", message: "Choose a Strong Password", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                            print("Okay Clicked.")
-                        }])
-                        
-                        self.passwordField.layer.cornerRadius = 08
-                        self.passwordField.layer.borderWidth = 3
-                        self.passwordField.layer.borderColor = K.colors.dangerColor
-                        self.passwordField.text = ""
-                        self.passwordField.placeholder = "Choose a Strong Password"
-                    }
-                } else {
-                    
-                    // Storing the user's data in the firebase cloud
-                    var ref: DocumentReference? = nil
-                    ref = self.db.collection("users").addDocument(data: [
-                        "username": username,
-                        "email": email,
-                        "country_code": countryCode,
-                        "phone_number": phoneNumber
-                    ]) { err in
-                        if let err = err {
-                            print("Error adding document: \(err)")
-                        } else {
-                            print("Document added with ID: \(ref!.documentID)")
+                        if !email.validateEmailId() {
+                            
+                            self.showAlert(title: AlertConstants.alertTitle, message: AlertConstants.invalidEmailMessage)
+                            self.emailTextField.layer.cornerRadius = 08
+                            self.emailTextField.layer.borderWidth = 3
+                            self.emailTextField.layer.borderColor = K.colors.dangerColor
+                            self.emailTextField.text = ""
+                            self.emailTextField.placeholder = AlertConstants.invalidEmailMessage
+                            
+                        } else if phoneNumber.count < 9 {
+                            
+                            self.showAlert(title: AlertConstants.alertTitle, message: AlertConstants.invalidMobileNoMessage)
+                            self.phoneNumberField.layer.cornerRadius = 08
+                            self.phoneNumberField.layer.borderWidth = 3
+                            self.phoneNumberField.layer.borderColor = K.colors.dangerColor
+                            self.phoneNumberField.text = ""
+                            self.phoneNumberField.placeholder = AlertConstants.invalidMobileNoMessage
+                            
+                        } else if self.reEnterPasswordField.text != self.passwordField.text {
+                            
+                            self.showAlert(title: AlertConstants.alertTitle, message: AlertConstants.passwordDoesntMatchMessage)
+                            self.reEnterPasswordField.layer.cornerRadius = 08
+                            self.reEnterPasswordField.layer.borderWidth = 3
+                            self.reEnterPasswordField.layer.borderColor = K.colors.dangerColor
+                            self.reEnterPasswordField.text = ""
+                            self.reEnterPasswordField.placeholder = AlertConstants.passwordDoesntMatchMessage
+                            
+                        } else if !password.validatePassword() {
+                            
+                            self.showAlert(title: AlertConstants.alertTitle, message: AlertConstants.chooseAStrongPasswordMessage)
+                            self.passwordField.layer.cornerRadius = 08
+                            self.passwordField.layer.borderWidth = 3
+                            self.passwordField.layer.borderColor = K.colors.dangerColor
+                            self.passwordField.text = ""
+                            self.passwordField.placeholder = AlertConstants.chooseAStrongPasswordMessage
+                            
                         }
+                        
+                    } else {
+                        
+                        // Storing the user's data in the firebase cloud
+                        var ref: DocumentReference? = nil
+                        ref = self.db.collection("users").addDocument(data: [
+                            "username": username,
+                            "email": email,
+                            "country_code": countryCode,
+                            "phone_number": phoneNumber
+                        ]) { err in
+                            if let err = err {
+                                print("Error adding document: \(err)")
+                            } else {
+                                print("Document added with ID: \(ref!.documentID)")
+                            }
+                        }
+                        self.performSegue(withIdentifier: K.segues.registerToHome, sender: self)
                     }
-                    self.performSegue(withIdentifier: K.segues.registerToHome, sender: self)
                 }
             }
-        } else {
-            self.openAlert(title: "Alert", message: "Kindly provide neccessary details", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                print("Okay Clicked.")
-            }])
         }
     }
 }
@@ -139,8 +160,9 @@ extension RegisterViewController: UITextFieldDelegate {
 }
 
 
-// MARK: - Country Code Picker Functionality & Basic Design
+// MARK: - Country Code Picker Functionality
 extension RegisterViewController: CountryPickerViewControllerDelegate {
+    
     func countryPickerViewControllerDidCancel(_ countryPickerViewController: Planet.CountryPickerViewController) {
         dismiss(animated: true, completion: nil)
     }
@@ -152,79 +174,12 @@ extension RegisterViewController: CountryPickerViewControllerDelegate {
     
     // Function that handles Country Code Functionality
     @objc func textDidBeginEditing(sender:UITextField) {
-       // handle begin editing event
+        // handle begin editing event
         let viewController = CountryPickerViewController()
         viewController.delegate = self
         
         let navigationController = UINavigationController(rootViewController: viewController)
         present(navigationController, animated: true, completion: nil)
-    }
-    
-    // Custom Design
-    func loadBasicDesigns() {
-        // Setting up the Icons for Emailand Passwords in the left.
-        emailIcon.image = UIImage(named: "mail")
-        userIcon.image = UIImage(named: "user")
-        lockIcon.image = UIImage(named: "lock")
-        lockIcon2.image = UIImage(named: "lock")
-        
-        let emailContentView = UIView()
-        let userContentView = UIView()
-        let passwordContentView = UIView()
-        let reEnterPasswordContentView = UIView()
-        
-        emailContentView.addSubview(emailIcon)
-        userContentView.addSubview(userIcon)
-        passwordContentView.addSubview(lockIcon)
-        reEnterPasswordContentView.addSubview(lockIcon2)
-        
-        emailContentView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        emailIcon.frame = CGRect(x: -15, y: 0, width: 20, height: 20)
-        
-        userContentView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        userIcon.frame = CGRect(x: -15, y: 0, width: 20, height: 20)
-        
-        passwordContentView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        lockIcon.frame = CGRect(x: -15, y: 0, width: 20, height: 20)
-        
-        reEnterPasswordContentView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        lockIcon2.frame = CGRect(x: -15, y: 0, width: 20, height: 20)
-        
-        
-        emailTextField.rightView = emailContentView
-        userNameTextField.rightView = userContentView
-        passwordField.rightView = passwordContentView
-        reEnterPasswordField.rightView = reEnterPasswordContentView
-        
-        emailTextField.rightViewMode = .always
-        userNameTextField.rightViewMode = .always
-        passwordField.rightViewMode = .always
-        reEnterPasswordField.rightViewMode = .always
-        
-        // Adding Boarders in Email and Password field
-        emailTextField.layer.cornerRadius = 08
-        emailTextField.layer.borderWidth = 3
-        emailTextField.layer.borderColor = K.colors.primaryColor
-        
-        userNameTextField.layer.cornerRadius = 08
-        userNameTextField.layer.borderWidth = 3
-        userNameTextField.layer.borderColor = K.colors.primaryColor
-        
-        countryCodeTextField.layer.cornerRadius = 08
-        countryCodeTextField.layer.borderWidth = 3
-        countryCodeTextField.layer.borderColor = K.colors.primaryColor
-        
-        phoneNumberField.layer.cornerRadius = 08
-        phoneNumberField.layer.borderWidth = 3
-        phoneNumberField.layer.borderColor = K.colors.primaryColor
-        
-        passwordField.layer.cornerRadius = 08
-        passwordField.layer.borderWidth = 3
-        passwordField.layer.borderColor = K.colors.primaryColor
-        
-        reEnterPasswordField.layer.cornerRadius = 08
-        reEnterPasswordField.layer.borderWidth = 3
-        reEnterPasswordField.layer.borderColor = K.colors.primaryColor
     }
     
 }
--- Vaccination NG/Vaccination NG/Controllers/ViewController.swift
+++ Vaccination NG/Vaccination NG/Controllers/ViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import FirebaseAuth
-import ProgressHUD
 
 class ViewController: UIViewController {
 
@@ -15,53 +14,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
-    let emailIcon = UIImageView()
-    let passwordIcon = UIImageView()
+    let loader = ProgressLoading()
+    
+    // Constants for alert titles and messages
+    enum AlertConstants {
+        static let alertTitle = "Alert"
+        static let invalidEmailPasswordMessage = "Enter Email and Password!"
+        static let invalidEmailMessage = "Email Address not found!"
+        static let wrongPasswordMessage = "Wrong Password"
+        static let emailNotFoundMessage = "Email not found"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Loading Basic Designs
-        loadBasicDesigns()
+        let customLoginVC = CustomLoginViewController()
+        customLoginVC.loadBasicDesigns(emailTextField, passwordField, loginButton)
     }
 
     @IBAction func loginPressed(_ sender: UIButton) {
+        
         if let email = emailTextField.text, let password = passwordField.text {
             Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
                 if let e = error {
                     print(e)
-                    if !email.validateEmailId() {
-                        
-                        self.openAlert(title: "Alert", message: "Email Address not found!", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                            print("Okay Clicked.")
-                        }])
-                        
+                    
+                    if !email.validateEmailId() && !password.validatePassword() {
+                        self.showAlert(title: AlertConstants.alertTitle, message: AlertConstants.invalidEmailPasswordMessage)
+                    } else if !email.validateEmailId() {
+                        self.showAlert(title: AlertConstants.alertTitle, message: AlertConstants.invalidEmailMessage)
                     } else if e.localizedDescription == "The password is invalid or the user does not have a password." {
-                        
-                        self.openAlert(title: "Alert", message: "Wrong Password", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                            print("Okay Clicked.")
-                        }])
-                        
+                        self.showAlert(title: AlertConstants.alertTitle, message: AlertConstants.wrongPasswordMessage)
                     } else if e.localizedDescription == "There is no user record corresponding to this identifier. The user may have been deleted." {
-                        
-                        self.openAlert(title: "Alert", message: "Email not found", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                            print("Okay Clicked.")
-                        }])
-                        
+                        self.showAlert(title: AlertConstants.alertTitle, message: AlertConstants.emailNotFoundMessage)
                     }
                 } else {
-//                    let progress = ProgressHUD.animationType = AnimationType.lineSpinFade
-                    ProgressHUD.colorAnimation = UIColor.systemBlue
-                    ProgressHUD.show()
+                    self.loader.progressLoading()
                     self.performSegue(withIdentifier: K.segues.loginToHome, sender: self)
                     print("LoggedIn")
                     
                 }
             }
-        } else {
-            self.openAlert(title: "Alert", message: "Kindly enter Email and Password", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{ _ in
-                print("Okay Clicked.")
-            }])
         }
     }
     
@@ -74,39 +68,11 @@ class ViewController: UIViewController {
         }
     }
     
-    func loadBasicDesigns() {
-        // Setting up the Icons for Emailand Passwords in the left.
-        emailIcon.image = UIImage(named: "mail")
-        passwordIcon.image = UIImage(named: "lock")
-        
-        let emailContentView = UIView()
-        let passwordContentView = UIView()
-        
-        emailContentView.addSubview(emailIcon)
-        passwordContentView.addSubview(passwordIcon)
-        
-        emailContentView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        emailIcon.frame = CGRect(x: -15, y: 0, width: 20, height: 20)
-        
-        passwordContentView.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        passwordIcon.frame = CGRect(x: -15, y: 0, width: 20, height: 20)
-        
-        emailTextField.rightView = emailContentView
-        emailTextField.rightViewMode = .always
-        
-        passwordField.rightView = passwordContentView
-        passwordField.rightViewMode = .always
-        
-        loginButton.titleLabel?.font = UIFont(name: K.colors.boldFont, size: 20)
-        
-        // Adding Boarders in Email and Password field
-        emailTextField.layer.cornerRadius = 08
-        emailTextField.layer.borderWidth = 3
-        emailTextField.layer.borderColor = K.colors.primaryColor
-        
-        passwordField.layer.cornerRadius = 08
-        passwordField.layer.borderWidth = 3
-        passwordField.layer.borderColor = K.colors.primaryColor
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
+    
 }
 
